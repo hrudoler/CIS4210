5aec8dcc9923cb9a5f6e5b0a08eec1b61b951955
hrudoler <91835213+hrudoler@users.noreply.github.com>
2022-08-31 15:30:33 -0400

    start hw1

--- python-skills.py
+++ python-skills.py
@@ -2,46 +2,61 @@
 # CIS 521: Python Skills Homework
 ############################################################
 
-student_name = "Type your full name here."
+student_name = "Helen Rudoler"
 
 # This is where your grade report will be sent.
-student_email = "Type your preferred email address here." 
+student_email = "hrudoler@seas.upenn.edu" 
 
 ############################################################
 # Section 1: Python Concepts
 ############################################################
 
-python_concepts_question_1 = "..."
+python_concepts_question_1 = """""
+    python is both statically and dynamically typed depending on the scenario. An example of dynamic typing is 
+    that the same variable can be redeclared to be a different type. 
+"""
 
-python_concepts_question_2 = "..."
+python_concepts_question_2 = """
+    Dictionary keys must be distinct an immutable. Therefore using a list as a key is not allowed as lists can be modified. 
+    A simple solution in this case would be to swap the key-value pairs, as strings can be used as keys (and a single location 
+    will never have more than one coordinate!)
+"""
 
-python_concepts_question_3 = "..."
+python_concepts_question_3 = """
+    Using join will be significantly faster. The for loop requires python to allocate space for each intermediary string as
+    more and more strings are added, but the using python's built in functionatiy can avoid this.
+"""
 
 ############################################################
 # Section 2: Working with Lists
 ############################################################
 
 def extract_and_apply(l, p, f):
-    pass
+    return [f(x) for x in l if p(x)]
 
 def concatenate(seqs):
+    # [for seq in seqs]
     pass
 
 def transpose(matrix):
-    pass
+    tp = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    for ind_i, i in matrix:
+        for ind_j, j in matrix[i]:
+            tp[ind_j][ind_i] = j
+    return tp
 
 ############################################################
 # Section 3: Sequence Slicing
 ############################################################
 
 def copy(seq):
-    pass
+    return seq[:]
 
 def all_but_last(seq):
-    pass
+    return seq[:-1]
 
 def every_other(seq):
-    pass
+    return seq[0:len(seq):2]
 
 ############################################################
 # Section 4: Combinatorial Algorithms
@@ -137,3 +152,16 @@ Type your response here.
 Your response may span multiple lines.
 Do not include these instructions in your response.
 """
+
+def main():
+    def test_bool(x):
+        if x % 2 == 0:
+            return True
+        return False
+    def test_func(x):
+        return x*x
+    extract_and_apply([1, 2, 3], test_bool, test_func)
+
+if __name__ == 'main':
+    main()
+    print("hello")
