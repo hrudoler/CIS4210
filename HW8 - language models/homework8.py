959245bebb7e9dde6e3eded248cd9a3fe0eebaf6
hrudoler <91835213+hrudoler@users.noreply.github.com>
2022-11-13 10:25:45 -0500

    language models

--- HW8 - language models/homework8.py
+++ HW8 - language models/homework8.py
@@ -88,45 +88,65 @@ class NgramModel(object):
 
 
     def random_token(self, context):
-        r = random.random()
+        if context not in self.n_gram_count:
+            return None
         tokens = list(self.n_gram_count[context].keys())
         weights = list(self.n_gram_count[context].values())
         return random.choices(tokens, weights)[0]
 
+
     def random_text(self, token_count):
-        pass
+        if self.n == 1: starting_context = ()
+        else: starting_context = tuple(["<START>" for x in range(self.n - 1)])
+        context = starting_context
+        out_string = ""
+        isnt_first = False
+        for _ in range(token_count):
+            if isnt_first:
+                out_string += " "
+            token = self.random_token(context)
+            out_string += (str(token))
+            if token == "<END>":
+                context = starting_context
+            elif self.n > 1:
+                context = context[1:] + (str(token),)
+            isnt_first = True
+        return out_string
 
     def perplexity(self, sentence):
-        pass
+        if self.n == 1: starting_context = ()
+        else: starting_context = tuple(["<START>" for x in range(self.n - 1)])
 
 def create_ngram_model(n, path):
-    pass
+    m = NgramModel(n)
+    with open(path) as f: 
+        sentences = f.readlines()
+        for sentence in sentences:
+            m.update(sentence)
+    return m
+    
 
 ############################################################
 # Section 2: Feedback
 ############################################################
 
-feedback_question_1 = 0
+feedback_question_1 = 3
 
 feedback_question_2 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
+I'm somewhat perplexed by the perplexity. 
 """
 
 feedback_question_3 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
+generating the realisic sentences was so cool!!
 """
 
 
 if __name__ == '__main__':
     # ngrams(3, ["a", "b", "c"])
-    m = NgramModel(2)
-    m.update("a b c d")
-    m.update("a b a b")
-    print(m.n_gram_count)
-    random.seed(2)
-    print([m.random_token(("b",))
-      for i in range(6)])
+    # m = NgramModel(2)
+    # m.update("a b c d")
+    # m.update("a b a b")
+    # random.seed(2)
+    # print(m.random_text(15))
+    m = create_ngram_model(3, "austen.txt")
+    print(m.random_text(30))
